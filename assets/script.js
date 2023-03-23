4305890dd5d82d4d658f16a21527ce02cccd0f37
[Nate Master] <[natejmaster@gmail.com]>
2023-03-23 11:30:33 -0500

    added JS notes and JS code

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+console.log('Here are the topics we learned through Prework:');
+function listTopics() {
+for (x = 0; x < topics.length; x++) {
+console.log(topics[x]);
+}
+}
+
+console.log('Which topic should we study first?');
+function selectTopic() {
+if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+}
+listTopics()
+selectTopic()
